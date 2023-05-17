14fa2d3c0566299aa2b0aa05cb47a64668003fa1
Monica Gerard <mlgvla@gmail.com>
2023-05-17 00:35:12 -0400

    adds conditional rendering of quotes

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,19 +1,22 @@
 import React from "react"
 import Quote from "./Quote"
 
-function Character({ character }) {
-
+function Character({ character, showAllQuotes = true }) {
    const { name, image, quotes } = character
 
-//    const quotePs = quotes.map((quote, index) => <p key={index}>{quote}</p>)
+   //    const quotePs = quotes.map((quote, index) => <p key={index}>{quote}</p>)
 
    return (
       <div className="card">
          <img src={image} alt={name} />
          <h2>{name}</h2>
          <Quote quote={quotes[0]} />
-         <Quote quote={quotes[1]} />
-         <Quote quote={quotes[2]} />
+         {showAllQuotes ? (
+            <>
+               <Quote quote={quotes[1]} />
+               <Quote quote={quotes[2]} />
+            </>
+         ) : null}
       </div>
    )
 }
