3edbbb39e9afb454f950423c77ff2235ee0c6d19
Monica Gerard <mlgvla@gmail.com>
2023-05-20 19:36:02 -0400

    start here

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import "./App.css"
 import { characters } from "./data"
-import Character from "./components/Character"
 
 function App() {
    return (
       <div className="App">
          <h1 className="main-title">The Simpsons</h1>
          <div className="container">
-            <Character character={characters[0]} />
-            <Character character={characters[1]} />
-            <Character character={characters[2]} />
-            <Character character={characters[3]} />
-            <Character character={characters[4]} />
-            <Character character={characters[5]} />
-            <Character character={characters[6]} />
-            <Character character={characters[7]} />
+            
          </div>
       </div>
    )
